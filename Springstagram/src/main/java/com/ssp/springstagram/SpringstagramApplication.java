c9da43372724d8499a775692ff2dbfb5da76c119
태라 <terra2007@naver.com>
2022-11-12 17:59:02 +0900

    ⚙️ :: 프로젝트 생성

---
+++ Springstagram/src/main/java/com/ssp/springstagram/SpringstagramApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.springstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstagramApplication.class, args);
+    }
+
+}
---
+++ Springstagram/src/test/java/com/ssp/springstagram/SpringstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp.springstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
